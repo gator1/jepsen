72b81a400ed9c191ce8faced7f34467d4b41553b
gary <gary@debian-msi>
2016-08-17 19:49:26 -0700

    add client-server test case

---
+++ fstorage/test/fstorage/cscp_test.clj
@@ -0,0 +1,36 @@
+(ns fstorage.cscp-test
+  (:require [clojure.test :refer :all]
+            [fstorage.core :refer :all]
+            [jepsen.core :as jepsen]
+            [jepsen.tests :as tests]
+            [jepsen.checker :as checker]
+            [jepsen.generator :as gen]))
+
+(defn fscscp-map
+  []
+  tests/noop-test
+  (assoc tests/noop-test
+    :name "fscscp-test"
+    :nodes [:n1 :n2 :n3]
+    :concurrency 5
+    :nemesis (partition-clients)
+    :generator (gen/phases
+                 (->> add
+                      (gen/stagger 1)
+                      (gen/nemesis
+                        (gen/seq (cycle [(gen/sleep 2)
+                                         {:type :info, :f :start}
+                                         (gen/sleep 2)
+                                         {:type :info, :f :stop}])))
+                      (gen/limit 20))
+                 (gen/nemesis
+                   (gen/once {:type :info :f :stop}))
+                 (gen/log "waiting for recover")
+                 (gen/sleep 5)
+                 (gen/clients (gen/once r)))
+    :checker checker/counter))
+
+; fstorage client-server consistency testing
+(deftest fscscp-test
+  (set-reg 0)
+  (is (:valid? (:results (jepsen/run! (fscscp-map))))))
---
+++ fstorage/test/fstorage/perf_test.clj
@@ -0,0 +1,33 @@
+(ns fstorage.perf-test
+  (:require [clojure.test :refer :all]
+            [fstorage.core :refer :all]
+            [jepsen.core :as jepsen]
+            [jepsen.generator :as gen]
+            [jepsen.tests :as tests]))
+
+(defn fsperf-map
+  [t]
+  tests/noop-test
+  (assoc tests/noop-test
+    :name "fsperf-test"
+    :concurrency 1
+    :nemesis (partition-node-n2)
+    :generator (->> w
+                    (gen/stagger 1)
+                    (gen/nemesis
+                      (gen/seq (cycle [(gen/sleep t)
+                                       {:type :info, :f :start}
+                                       (gen/sleep t)
+                                       {:type :info, :f :stop}])))
+                    (gen/limit 10))
+    :checker perf-checker)
+  )
+
+; fstorage performance testing
+; testcase 1: n2 out for 2s
+(deftest fsperf-test-1
+  (is (:valid? (:results (jepsen/run! (fsperf-map 2))))))
+
+; testcase 2: n2 out for 6s, which causes downgrade
+(deftest fsperf-test-2
+  (is (:valid? (:results (jepsen/run! (fsperf-map 6))))))
