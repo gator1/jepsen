a93d61a30f898ef0bf2c78f4104c7a0e975cfe98
gary <gary@debian-jepsen1>
2016-09-14 13:08:56 -0700

    kafka topic works stii hangs

--- kafka/src/jepsen/kafka.clj
+++ kafka/src/jepsen/kafka.clj
@@ -14,7 +14,9 @@
   []
   (Thread/sleep 20)
   (info "creating topic")
-  (c/exec (c/lit "/opt/kafka/bin/kafka-create-topic.sh --partition 5 --replica 5 --topic jepsen --zookeeper localhost:2181 ")))
+  (c/exec (c/lit "/opt/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic jepsen "))
+  (info "creating topic done")
+)
 
 (defn start!
   [id]
@@ -48,11 +50,16 @@
     ;(c/exec :rm :-rf "/opt/kafka")
     (c/exec :rm :-rf "/tmp/kafka-logs")))
 
-(defn set-broker-id!  [filename id]
-  (let [transform #(if  (clojure.string/starts-with? %1 "broker.id")  (str "broker.id=" id) %1)
-        joined (clojure.string/join "\n" (map transform (-> filename slurp clojure.string/split-lines)))]
-    (info "joined: " joined)
-    (spit filename joined)))
+(comment
+  (defn set-broker-id!  [filename id]
+    (info "set-broker-id!: filename: " filename)
+    (let [transform #(if  (clojure.string/starts-with? %1 "broker.id")  (str "broker.id=" id) %1)
+          joined (clojure.string/join "\n" (map transform (-> filename slurp clojure.string/split-lines)))]
+      (info "joined: " joined)
+      (spit filename joined))))
+
+(defn set-broker-id! [filename id]
+   (c/exec  (c/lit  (format "sed -i.bak '/^broker\\.id/s/^.*$/broker.id=%s/' %s"     id filename))))
 
 (comment
   (defn set-broker-id!  [filename id]
@@ -65,29 +72,33 @@
 
 (defn deploy [id node version]
   (let [filename "/opt/kafka/config/server.properties"]
-    (info "set-broker-id!" filename node id )
+    ;(info "set-broker-id!" filename node id )
     (set-broker-id! filename id))
-  ; Create topic asynchronously
-  (future  (create-topic))
-  (start! id))
+    ;(info "set-broker-id done!" id )
+    (start! id)
+    ; Create topic asynchronously
+    (when (= id 1)
+       (future  (create-topic)))
+)
 
 (defn install! [node version]
    ; Install specific versions
   (info "install! Kafka" node )
   (let [id  (Integer.  (re-find #"\d+", (name node)))
         kafka "kafka_2.10-0.10.0.1"]
-     (info node id)
-     (c/exec :apt-get :update)
-     (c/exec :apt-get :install :-y :--force-yes "default-jre")
-     (c/exec :apt-get :install :-y :--force-yes "wget")
-     (c/exec :rm :-rf "/opt/kafka")
-     (c/exec :mkdir :-p "/opt/kafka")
-     (c/cd "/opt/"
-           (c/exec :wget (format "http://apache.claz.org/kafka/0.10.0.1/%s.tgz" kafka))
-           (c/exec :gzip :-d (format "%s.tgz" kafka))
-           (c/exec :tar :xf (format "%s.tar" kafka))
-           (c/exec :mv kafka "kafka"))
-     (deploy id node version)))
+    (info node id)
+    (c/exec :apt-get :update)
+    (c/exec :apt-get :install :-y :--force-yes "default-jre")
+    (c/exec :apt-get :install :-y :--force-yes "wget")
+    (c/exec :rm :-rf "/opt/")
+    (c/exec :mkdir :-p "/opt/")
+    (c/cd "/opt/"
+          (c/exec :wget (format "http://apache.claz.org/kafka/0.10.0.1/%s.tgz" kafka))
+          (c/exec :gzip :-d (format "%s.tgz" kafka))
+          (c/exec :tar :xf (format "%s.tar" kafka))
+          (c/exec :mv kafka "kafka")
+          (c/exec :rm (format "%s.tar" kafka)))
+    (deploy id node version)))
 
 (defn db
     "Kafka DB for a particular version."
@@ -95,7 +106,7 @@
     (let [zk (zk/db "3.4.5+dfsg-2")]
       (reify db/DB
         (setup!  [_ test node]
-          (info node "installing zk" version)
+          (info node "installing zk Node 1 only" version)
           (db/setup! zk test node)
           (info node "installing kafka" version)
           (install!  node version))
