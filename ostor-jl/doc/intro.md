7310206ecdb6f0f69fe7b15c3e5b7df11da8ff14
Jonathan Leonard <johanatan@gmail.com>
2016-09-28 23:39:44 -0700

    Add ostor-jl.

---
+++ ostor-jl/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ostor-jl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ ostor-jl/test/ostor_jl/core_test.clj
@@ -0,0 +1,32 @@
+(ns ostor-jl.core-test
+  (:require [clojure.test :refer :all]
+            [ostor-jl.core :refer :all]
+            [jepsen.core :as jepsen]
+            [jepsen.generator :as gen]
+            [jepsen.checker :as checker]
+            [jepsen.tests :as tests]
+            [knossos.model :refer [cas-register]])
+  (:use     clojure.tools.logging))
+
+(deftest oscp-test
+  (info "consistency test\n")
+  (set-reg 0)
+  (let [test (assoc tests/noop-test
+               :nodes [:n1 :n2]
+               :name "osuni-cap-test"
+               :concurrency 3
+               :client (client)
+               :nemesis (partition-uni)
+               :generator (->> (gen/mix [r w cas])
+                               (gen/stagger 1)
+                               (gen/nemesis
+                                 (gen/seq (cycle [(gen/sleep 5)
+                                                  {:type :info, :f :start}
+                                                  (gen/sleep 5)
+                                                  {:type :info, :f :stop}])))
+                               (gen/time-limit 100))
+               :model (cas-register 0)
+               :checker (checker/compose
+                          {:perf   (checker/perf)
+                           :linear checker/linearizable}))]
+    (is (:valid? (:results (jepsen/run! test))))))
