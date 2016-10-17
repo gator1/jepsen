f6a90a5cff30e36113398ba7465aa92e3ff6c224
Jonathan Leonard <johanatan@gmail.com>
2016-10-17 14:24:48 -0700

    Implement realistic nemesis and failure modes.

--- ostor-jl/src/ostor_jl/core.clj
+++ ostor-jl/src/ostor_jl/core.clj
@@ -9,9 +9,9 @@
                     [tests     :as tests]
                     [util      :refer [timeout]]]
             [jepsen.control.net :as control.net])
-  (:refer-clojure :exclude [read write]))
+  (:refer-clojure :exclude [read write] :rename {set cset}))
 
-(def ^:private disk-size 48)
+(def ^:private disk-size 128)
 (def ^:private cache-size (/ disk-size 2))
 
 (def ^:dynamic *array-size* 2)
@@ -36,94 +36,131 @@
       (write [_ address value]
         (swap! backing-store assoc address value)))))
 
-(def ^:private ostor-noop-disk
-  (reify MemoryStore
-    (read [_ _] nil)
-    (write [_ _ _] nil)))
-
 (defprotocol Sync
   (set [this address value]))
+
+(def ^:private timeout-interval 5000)
+
+(defprotocol NemesisHook
+  (disable [_ idx])
+  (enable [_ idx]))
 
 (defn- lru-cache [size backing-store]
   (let [cache (proxy [java.util.LinkedHashMap] [(+ 1 size) 2.0 true]
                 (removeEldestEntry [e] (> (proxy-super size) size)))
+        disabled? (atom false)
         store (atom backing-store)]
     (reify
       MemoryStore
       (read [_ address]
-        (locking cache
-          (if (.containsKey cache address)
-            (.get cache address)
-            (.put cache address (.read @store address)))))
+        (if @disabled? (Thread/sleep timeout-interval)
+            (locking cache
+              (if (.containsKey cache address)
+                (.get cache address)
+                (.put cache address (read @store address))))))
       (write [_ address value]
-        (locking cache
-          (.write @store address value)
-          (.put cache address value)))
+        (if @disabled? (Thread/sleep timeout-interval)
+            (locking cache
+              (write @store address value)
+              (.put cache address value))))
       HotSwap
       (swap [_ new-store] (reset! store new-store))
+      NemesisHook
+      (disable [_ _] (reset! disabled? true))
+      (enable [_ _] (.clear cache) (reset! disabled? false))
       Sync
       (set [_ address value]
-        (.put cache address value)))))
+        (if (not @disabled?)
+          (.put cache address value))))))
 
 (defn- translate-address [segment-size segment-vector address]
   [(segment-vector (quot address segment-size)) (rem address segment-size)])
 
 (defn- create-tandem [disk1 disk2]
   (let [cache-a1 (lru-cache cache-size disk1)
-        cache-a2 (lru-cache cache-size ostor-noop-disk)
-        cache-b1 (lru-cache cache-size ostor-noop-disk)
-        cache-b2 (lru-cache cache-size disk2)]
-    (letfn [(disk-and-caches [address]
-              (translate-address disk-size [[disk1 cache-a1 cache-b1]
-                                            [disk2 cache-b2 cache-a2]] address))]
-      (reify MemoryStore
+        cache-a2 (lru-cache cache-size nil)
+        cache-b1 (lru-cache cache-size nil)
+        cache-b2 (lru-cache cache-size disk2)
+        caches (atom {disk1 {:primary cache-a1 :replica cache-b1}
+                      disk2 {:primary cache-b2 :replica cache-a2}})]
+    (letfn [(address->disk-offset [address]
+              (translate-address disk-size [disk1 disk2] address))
+            (operate [disk offset operation]
+              (locking disk
+                (let [[cache1 cache2] [(get-in @caches [disk :primary]) (get-in @caches [disk :replica])]
+                      res (operation cache1)]
+                  (if cache2 (set cache2 offset res))
+                  res)))
+            (disable- [disk caches-to-disable cache-to-take-disk]
+              (locking disk
+                (doseq [cache caches-to-disable] (disable cache nil))
+                (swap cache-to-take-disk disk)
+                (swap! caches assoc disk {:primary (get-in @caches [disk :replica]) :replica nil})))
+            (enable- [disk caches-to-enable cache-to-take-disk]
+              (locking disk
+                (swap cache-to-take-disk disk)
+                (doseq [cache caches-to-enable] (enable cache nil))
+                (swap! caches assoc disk {:replica (get-in @caches [disk :primary])
+                                          :primary cache-to-take-disk})))]
+      (reify
+        MemoryStore
         (read [_ address]
-          (let [[[disk cache1 cache2] offset] (disk-and-caches address)]
-            (locking disk
-              (let [res (.read cache1 offset)]
-                (.set cache2 offset res)
-                res))))
+          (let [[disk offset] (address->disk-offset address)]
+            (operate disk offset #(read % offset))))
         (write [_ address value]
-          (let [[[disk cache1 cache2] offset] (disk-and-caches address)]
-            (locking disk
-              (let [res (.write cache1 offset value)]
-                (.set cache2 offset res)
-                res))))))))
+          (let [[disk offset] (address->disk-offset address)]
+            (operate disk offset #(write % offset value))))
+        NemesisHook
+        (disable [_ idx]
+          (case idx
+            0 (disable- disk1 [cache-a1 cache-a2] cache-b1)
+            1 (disable- disk2 [cache-b2 cache-b1] cache-a2)))
+        (enable [_ idx]
+          (case idx
+            0 (enable- disk1 [cache-a1 cache-a2] cache-a1)
+            1 (enable- disk2 [cache-b2 cache-b1] cache-b2)))))))
+
+(def ^:private disks (delay (repeatedly *array-size* ostor-disk)))
+(def ^:private cache (delay (apply create-tandem @disks))) ; NOTE assumes *array-size* equals 2
 
 (defn ostor-simulator []
-  (let [disk1 (ostor-disk)
-        disk2 (ostor-disk)
-        cache (create-tandem disk1 disk2)]
-    (reify client/Client
-      (setup! [this test node]
-        this)
-      (invoke! [this test op]
-        (timeout 5000 (assoc op :type :info :error :timeout)
-                 (case (:f op)
-                   :txn (let [[[operator offset value]] (:value op)]
-                          (case operator
-                            :read (assoc op :type :ok :value [[:read offset (.read cache offset)]])
-                            :write (do (.write cache offset value)
-                                       (assoc op :type :ok :value [[:write offset value]])))))))
-      (teardown! [_ test]))))
-
-(defn- linkdown [node]
-
-  )
-
-(defn- linkup [node]
-
-  )
-
-(defn nemesis []
   (reify client/Client
-    (setup! [this test _]
+    (setup! [this test node]
       this)
     (invoke! [this test op]
-      (case (:f op)
-        :start (do (linkdown (first (:nodes test)))
-                   (assoc op :value "Cut off PCIE link"))
-        :stop  (do (linkup (first (:nodes test)))
-                   (assoc op :value "fully connected"))))
-    (teardown! [this test]
-      (linkup (first (:nodes test))))))
+      (timeout timeout-interval (assoc op :type :info :error :timeout)
+               (case (:f op)
+                 :txn (let [[[operator offset value]] (:value op)]
+                        (case operator
+                          :read (assoc op :type :ok :value [[:read offset (read @cache offset)]])
+                          :write (do (write @cache offset value)
+                                     (assoc op :type :ok :value [[:write offset value]])))))))
+    (teardown! [_ test])))
+
+(defn- linkdown [idx]
+  (disable @cache idx))
+
+(defn- linkup [idx]
+  (enable @cache idx))
+
+(defn nemesis []
+  (let [down (atom #{})
+        do-link (fn [op idx swap-op link-fn msg]
+                  (assoc op :value
+                         (if idx
+                           (do
+                             (link-fn idx)
+                             (swap! down swap-op idx)
+                             (format "Controller: %s %s" idx msg))
+                           (format "All controllers already %s" msg))))]
+    (reify client/Client
+      (setup! [this test _]
+        this)
+      (invoke! [this test op]
+        (case (:f op)
+          :start (let [idx (first (take 1 (shuffle (clojure.set/difference (cset (range *array-size*)) @down))))]
+                   (do-link op idx conj linkup "up"))
+          :stop  (let [idx (first (take 1 (shuffle @down)))]
+                   (do-link op idx disj linkdown "down"))))
+      (teardown! [this test]
+        (linkup 0)))))
--- ostor-jl/test/ostor_jl/core_test.clj
+++ ostor-jl/test/ostor_jl/core_test.clj
@@ -6,24 +6,28 @@
             [jepsen.checker :as checker]
             [jepsen.tests :as tests]
             [knossos.model :refer [multi-register]])
-  (:use     clojure.tools.logging))
+  (:use     clojure.tools.logging)
+  (:refer-clojure :exclude [read write set]))
 
 (deftest oscp-test
   (info "consistency test\n")
-  (let [test (assoc tests/noop-test
+  (let [time-limit 60
+        test (assoc tests/noop-test
                :nodes [:n1 :n2 :n3 :n4 :n5]
                :name "osuni-cap-test"
                :concurrency 5
                :client (ostor-simulator)
                :nemesis (nemesis)
-               :generator (->> (gen/mix [w r])
-                               (gen/stagger 1)
-                               (gen/nemesis
-                                 (gen/seq (cycle [(gen/sleep 5)
-                                                  {:type :info, :f :start}
-                                                  (gen/sleep 5)
-                                                  {:type :info, :f :stop}])))
-                               (gen/time-limit 6))
+               :generator (->> (gen/mix [w])
+                               (gen/stagger 5)
+                               (gen/nemesis (gen/seq (shuffle
+                                                      (mapcat identity
+                                                              (repeatedly time-limit
+                                                                          #(let [sleep (+ 1 (rand-int 5))]
+                                                                             [{:type :info :f :stop}
+                                                                              (gen/sleep sleep)
+                                                                              {:type :info :f :start}]))))))
+                               (gen/time-limit time-limit))
                :model (multi-register {})
                :checker (checker/compose
                           {:perf   (checker/perf)
