0ffb6a575203159261ece8a99a36710fed85003d
Kevin DeGraaf <kevin@debian-msi>
2017-04-03 18:41:20 -0700

    etcdemo from scaffold.

---
+++ etcdemo/project.clj
@@ -0,0 +1,9 @@
+(defproject jepsen.etcdemo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main jepsen.etcdemo
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jepsen "0.1.4"]
+                 [verschlimmbesserung "0.1.3"]])
