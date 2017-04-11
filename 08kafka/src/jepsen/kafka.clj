613a470460bb5da767763b95a315860692244be4
Kevin DeGraaf <khdegraaf@gmail.com>
2017-04-10 17:53:39 -0700

    nuke

--- 08kafka/src/jepsen/kafka.clj
+++ 08kafka/src/jepsen/kafka.clj
@@ -132,11 +132,11 @@
           (info "setup! kafka done"  node)
         )
         (teardown!  [_ test node]
-          ;(info "tearing down Zookeeper")
-          ;(db/teardown! zk test node)
-          ;(info "tearing down Kafka NUKE!!!" node)
-          ;(nuke!)
-          ;(info "Kafka NUKED!!!" node)
+          (info "tearing down Zookeeper")
+          (db/teardown! zk test node)
+          (info "tearing down Kafka NUKE!!!" node)
+          (nuke!)
+          (info "Kafka NUKED!!!" node)
           ))))
 
 
