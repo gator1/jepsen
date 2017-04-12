69c98dd1b9b68ac79d231971648a373f9e1d7e29
miket2017 <gary@debian-jepsen1>
2017-04-11 18:35:29 -0700

    Add more nemesis

--- file-e/src/file_e/nemesis.clj
+++ file-e/src/file_e/nemesis.clj
@@ -10,8 +10,13 @@
              ])
   (:use     [clojure.java.shell :only [sh]]) )
 
-(defn noop-nemesis
-  "Does nothing."
+(def pwd-sudo "ubuntu")
+(defn sudo-cmd
+  [cmd]
+  (sh "sh" "-c" (str "echo " pwd-sudo  " | sudo -S " cmd)))
+
+(defn nemesis-test-1
+  "Write wrong value."
   []
   (reify client/Client
     (setup! [this test node] this)
@@ -22,5 +27,40 @@
                      v (+ 5 (rand-int 5))]
                  (set-reg loc v)
                  (assoc op :value (str "Write " v " into " loc)))
-        :stop  (assoc op :value (str "stop noop-nemesis >>>>> "))))
+        :stop  (assoc op :value (str "stop nemesis-test-1 >>>>> "))))
     (teardown! [this test] this)))
+
+(defn nemesis-test-2
+  "Reboot one node"
+  []
+  (reify client/Client
+    (setup! [this test node] this)
+    (invoke! [this test op]
+      (case (:f op)
+        :start (let [node (str "n5")]
+                 (do
+                   (c/on node (c/su (c/exec :shutdown :-r :now)))
+                   (assoc op :value (str "Rebooted " node)))
+        :stop  (assoc op :value (str "stop nemesis-test-2 >>>>> ")))))
+    (teardown! [this test] this)))
+
+(def MDS {:n4 "n4" :n5 "n5"})
+(def mds (atom nil))
+(defn nemesis-test-3
+  "Shutdown one selected mds, then start it"
+  []
+  (reify client/Client
+    (setup! [this test node] this)
+    (invoke! [this test op]
+      (case (:f op)
+        :start (do
+                 (reset! mds (rand-nth (keys MDS)))
+                 (c/on (@mds MDS)  (c/su (c/exec :shutdown :-h :now))
+                 (assoc op :value (str "shutdown " @mds))))
+        :stop  (let [ret (sudo-cmd (str "lxc-start -d -n " (@mds MDS)))
+                     _ (println (str "exit - " (:exit ret) " err - " (:err ret)))]
+                 (if (= 0 (:exit ret)) 
+                   (assoc op :value (str "start " (@mds MDS) " success"))
+                   (assoc op :value (str "start " (@mds MDS) " fail"))))))
+    (teardown! [this test] this)))
+
