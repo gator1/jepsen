f3651d18f1dc25eb8f960274cf3ed12c9468e638
Kevin DeGraaf <khdegraaf@gmail.com>
2017-04-28 14:11:38 -0700

    better docs, fixed tests.

--- file-e/README.md
+++ file-e/README.md
@@ -10,16 +10,16 @@ You can run all tests by executing
     
 All of the results and analytics can be nicely viewed by executing
    
-    lein run server
+    lein run serve
     
 in your docker compose container.  This website should be visible via the url:
     
     http://172.18.0.8:8080
     
 all of your test runs since the creation of the container will be there with full
-logging and analytical info.  There is a chance the ip # is changed, and if so it
-can be determined by installing net-tools (apt-get install net-tools) in the control
-node and then running the command
+logging and analytical info.  There is a chance the ip # is changed (although it has
+always been the same for me), and if so it can be determined by installing net-tools 
+(apt-get install net-tools) in the control node and then running the command
 
     ifconfig
     
--- file-e/test/file_e/core_test.clj
+++ file-e/test/file_e/core_test.clj
@@ -12,49 +12,10 @@
             [knossos.model :refer [cas-register, multi-register]])
   (:use     clojure.tools.logging))
 
-(deftest file-nemesis-test
-  (info "noop test\n")
-  (let [test (assoc tests/noop-test
-                    :nodes [:n1 :n2 :n3]
-                    :name "file-nemesis-test"
-                    :net file-iptables
-                    :client (client-nfs nil)
-                    ; :nemesis (nemesis/partition-random-node)
-                    ;:nemesis (nemesis-test-2)  ; reboot node
-                    ;:nemesis (nemesis-test-3)  ; reboot node
-                    ;:nemesis (nemesis-test-7)  ; block node's port
-                    :nemesis (nemesis-test-9)  ; kill process
-                    :generator (->> (gen/mix [r w cas])
-                                 (gen/stagger 1)
-                                 (gen/nemesis
-                                   (gen/seq (cycle [(gen/sleep 5)
-                                                    {:type :info, :f :start}
-                                                    (gen/sleep 5)
-                                                    {:type :info, :f :stop}])))
-                                 (gen/time-limit 20))
-                    :model (cas-register 0)
-                    :checker (checker/compose
-                               {:perf   (checker/perf)
-                                :linear checker/linearizable}))]
+(deftest do-file-nemesis-test
+  (let [test (file-nemesis-test {})]
     (is (:valid? (:results (jepsen/run! test))))))
 
-(deftest file-cap-multi-test
-  (info "consistency mutli-register test\n")
-  (init-multi-data 1 4096)
-  (let [test (assoc tests/noop-test
-               :nodes [:n1 :n2 :n3]
-               :name "file-cap-multi-test"
-               :client (client-multi nil)
-               :nemesis (nemesis/partition-random-halves)
-               :generator (->> (gen/mix [rm wm])
-                               (gen/stagger 1)
-                               ;(gen/clients)
-                               (gen/nemesis
-                                 (gen/seq (cycle [(gen/sleep 5)
-                                                  {:type :info, :f :start}
-                                                  (gen/sleep 5)
-                                                  {:type :info, :f :stop}])))
-                               (gen/time-limit 10))
-               :model (multi-register (zipmap key-list (repeat 0)))
-               :checker (independent/checker checker/linearizable))]
+(deftest do-file-cap-multi-test
+  (let [test (file-cap-multi-test {})]
     (is (:valid? (:results (jepsen/run! test))))))
